d21dce8bfda5b227d02793ecc145f1f1d5a4a87a
CLEBER MARQUES DE JESUS <cleberjesus@pge.sc.gov.br>
2024-12-09 15:45:34 -0300

    Primeiro commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Banner />
+    <div class="main-container">
+      <h1>Monte seu burguer:</h1>
+      <Burgerform />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Banner from "../components/Banner.vue";
+import Burgerform from "../components/Burgerform.vue";
 
 export default {
-  name: 'HomeView',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Banner,
+    Burgerform,
+  },
+};
 </script>
